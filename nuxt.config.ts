78d3c633dc992b054fe24dfc9ce502c5bf9781e6
Ilya <85136486+UchVuch@users.noreply.github.com>
2024-01-26 04:00:37 +0300

    try to add robots/sitemap to display dynamic pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
-    '@nuxtjs/eslint-module'
-  ]
+    '@nuxtjs/eslint-module',
+    'nuxt-simple-robots',
+    '@nuxtjs/sitemap'
+  ],
+  site: {
+    url: 'https://nuxt-dzen.vercel.app/'
+  }
 })
