8e50bd3ff685cbee7098f8ae59f5d3bac7bd2a25
Ilya <85136486+UchVuch@users.noreply.github.com>
2024-01-26 05:30:08 +0300

    Add files via upload

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,5 +1,4 @@
 <script setup>
-const data = ref([])
 const route = useRoute()
 const product = ref({
   name: '',
@@ -8,52 +7,61 @@ const product = ref({
   description: '',
   image: ''
 })
-function fetchData () {
-  setTimeout(() => {
-    data.value = [
-      {
-        id: 61166,
-        name: 'DS-H104G 4-х канальный гибридный HD-TVI регистратор',
-        firm: 'HiWatch',
-        group: 'ВИДЕОРЕГИСТРАТОРЫ HD-TVI',
-        description: 'DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353',
-        image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
-      },
-      {
-        id: 64452,
-        name: 'Сетевой неуправляемый коммутатор',
-        firm: 'HiWatch',
-        group: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
-        description: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
-        image: ''
-      },
-      {
-        id: 64454,
-        name: 'Комплект аналогового видеодомофона  ',
-        firm: 'HiWatch',
-        group: 'ДОМОФОНЫ',
-        description: 'HiWatch "DS-D100KF Комплект аналогового видеодомофона  c памятью до 200 снимков" 305303182',
-        image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
-      }
-    ]
-    console.log(data.value)
-    const currentProduct = data.value.find(item => item.id === Number(route.params.id))
-    product.value = currentProduct
-    useSeoMeta({
-      title: `${product.value.name} | Dzen`,
-      ogTitle: `${product.value.name} | Dzen`,
-      description: `
-        ${product.value.description}. ${product.value.group}. ${product.value.firm}. Липецк. ${product.value.description}. ${product.value.group}. ${product.value.firm} в Липецке.
-      `,
-      ogDescription: `
-        ${product.value.description}. ${product.value.group}. ${product.value.firm}. Липецк. ${product.value.description}. ${product.value.group}. ${product.value.firm} в Липецке.
-      `,
-      ogImage: product.value.image,
-      twitterCard: 'product'
-    })
-  }, 1000)
-}
-fetchData()
+const {data} = useFetch('https://fakerapi.it/api/v1/products?_quantity=1&_taxes=12&_categories_type=uuid', {
+  pick: ['data']
+})
+useSeoMeta({
+  title: `DS-H104G 4-х канальный гибридный HD-TVI регистратор | Dzen`,
+  ogTitle: `DS-H104G 4-х канальный гибридный HD-TVI регистратор | Dzen`,
+  description: `DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353. DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353 в Липецке. Липецк`,
+  ogDescription:  `DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353. DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353 в Липецке. Липецк`
+})
+// function fetchData () {
+//   setTimeout(() => {
+//     data.value = [
+//       {
+//         id: 61166,
+//         name: 'DS-H104G 4-х канальный гибридный HD-TVI регистратор',
+//         firm: 'HiWatch',
+//         group: 'ВИДЕОРЕГИСТРАТОРЫ HD-TVI',
+//         description: 'DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353',
+//         image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
+//       },
+//       {
+//         id: 64452,
+//         name: 'Сетевой неуправляемый коммутатор',
+//         firm: 'HiWatch',
+//         group: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
+//         description: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
+//         image: ''
+//       },
+//       {
+//         id: 64454,
+//         name: 'Комплект аналогового видеодомофона  ',
+//         firm: 'HiWatch',
+//         group: 'ДОМОФОНЫ',
+//         description: 'HiWatch "DS-D100KF Комплект аналогового видеодомофона  c памятью до 200 снимков" 305303182',
+//         image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
+//       }
+//     ]
+//     console.log(data.value)
+//     const currentProduct = data.value.find(item => item.id === Number(route.params.id))
+//     product.value = currentProduct
+//     useSeoMeta({
+//       title: `${product.value.name} | Dzen`,
+//       ogTitle: `${product.value.name} | Dzen`,
+//       description: `
+//         ${product.value.description}. ${product.value.group}. ${product.value.firm}. Липецк. ${product.value.description}. ${product.value.group}. ${product.value.firm} в Липецке.
+//       `,
+//       ogDescription: `
+//         ${product.value.description}. ${product.value.group}. ${product.value.firm}. Липецк. ${product.value.description}. ${product.value.group}. ${product.value.firm} в Липецке.
+//       `,
+//       ogImage: product.value.image,
+//       twitterCard: 'product'
+//     })
+//   }, 1000)
+// }
+// fetchData()
 </script>
 <template>
   <div>
@@ -61,19 +69,19 @@ fetchData()
       <template #header>
         <Placeholder class="h-8">
           <h2>
-            {{ product.name }}
+            {{ data.data[0].name }}
           </h2>
         </Placeholder>
       </template>
 
       <Placeholder class="h-32">
-        <img :src="product.image" alt="item image" title="item.name" class="w-40 h-40 object-cover rounded">
+        <img :src="data.data[0].image" alt="item image" title="item.name" class="w-40 h-40 object-cover rounded">
       </Placeholder>
 
       <template #footer>
         <Placeholder class="h-8">
           <p>
-            {{ product.description }}
+            {{ data.data[0].description }}
           </p>
         </Placeholder>
       </template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,45 +5,75 @@ useSeoMeta({
   description: 'Dzen агрегатор Липецк. Камеры для наблюдения за территориeй. Камеры для домофонов. Регистраторы. Коммутаторы. Видеодомофоны. Камеры для наблюдения за территориeй Липецк. Камеры для домофонов Липецк. Регистраторы Липецк. Коммутаторы Липецк. Видеодомофоны Липецк. Найти товары в Липецке. Агрегатор Липецк. Найти товары агрегатор Липецк.',
   ogDescription: 'Dzen агрегатор Липецк. Камеры для наблюдения за территориeй. Камеры для домофонов. Регистраторы. Коммутаторы. Видеодомофоны. Камеры для наблюдения за территориeй Липецк. Камеры для домофонов Липецк. Регистраторы Липецк. Коммутаторы Липецк. Видеодомофоны Липецк. Найти товары в Липецке. Агрегатор Липецк. Найти товары агрегатор Липецк.'
 })
-const {data, pending} = useFetch('https://fakerapi.it/api/v1/products?_quantity=1&_taxes=12&_categories_type=uuid', {
+const {data} = useFetch('https://fakerapi.it/api/v1/products?_quantity=1&_taxes=12&_categories_type=uuid', {
   pick: ['data']
 })
-// function fetchData () {
-//   setTimeout(() => {
-//     items.value = [
-//       {
-//         id: 61166,
-//         name: 'DS-H104G 4-х канальный гибридный HD-TVI регистратор',
-//         firm: 'HiWatch',
-//         group: 'ВИДЕОРЕГИСТРАТОРЫ HD-TVI',
-//         description: 'DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353',
-//         image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
-//       },
-//       {
-//         id: 64452,
-//         name: 'Сетевой неуправляемый коммутатор',
-//         firm: 'HiWatch',
-//         group: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
-//         description: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
-//         image: ''
-//       },
-//       {
-//         id: 64454,
-//         name: 'Комплект аналогового видеодомофона  ',
-//         firm: 'HiWatch',
-//         group: 'ДОМОФОНЫ',
-//         description: 'HiWatch "DS-D100KF Комплект аналогового видеодомофона  c памятью до 200 снимков" 305303182',
-//         image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
-//       }
-//     ]
-//   }, 1000)
-// }
-// fetchData()
+const items = ref([])
+function fetchData () {
+  setTimeout(() => {
+    items.value = [
+      {
+        id: 61166,
+        name: 'DS-H104G 4-х канальный гибридный HD-TVI регистратор',
+        firm: 'HiWatch',
+        group: 'ВИДЕОРЕГИСТРАТОРЫ HD-TVI',
+        description: 'DS-H104G HD-TVI регистратор; гибридный; 4-х канальный; AHD; TVI; CVI: 1080p Lite/ 720p@25к/с; 1 SATA; 1 IP-канал; 12В DC;  8Вт; HiWatch 300217353',
+        image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
+      },
+      {
+        id: 64452,
+        name: 'Сетевой неуправляемый коммутатор',
+        firm: 'HiWatch',
+        group: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
+        description: 'HiWatch  DS-S604P Сетевой неуправляемый коммутатор 301802013',
+        image: ''
+      },
+      {
+        id: 64454,
+        name: 'Комплект аналогового видеодомофона  ',
+        firm: 'HiWatch',
+        group: 'ДОМОФОНЫ',
+        description: 'HiWatch "DS-D100KF Комплект аналогового видеодомофона  c памятью до 200 снимков" 305303182',
+        image: 'https://crdx.ru/upload/goods/061/166/big/DS-H104G.jpg'
+      }
+    ]
+  }, 1000)
+}
+fetchData()
 </script>
 <template>
   <div class="grid grid-cols-1 gap-4">
     <ULink
       v-for="item in data.data"
+      :key="item.id"
+      :to="`/${item.id}`"
+      active-class="text-primary"
+      inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+    >
+      <UCard>
+        <template #header>
+          <Placeholder class="h-8">
+            <h2>
+              {{ item.name }}
+            </h2>
+          </Placeholder>
+        </template>
+
+        <Placeholder class="h-32">
+          <img :src="item.image" alt="item image" title="item.name" class="w-40 h-40 object-cover rounded">
+        </Placeholder>
+
+        <template #footer>
+          <Placeholder class="h-8">
+            <p>
+              {{ item.description }}
+            </p>
+          </Placeholder>
+        </template>
+      </UCard>
+    </ULink>
+    <ULink
+      v-for="item in items"
       :key="item.id"
       :to="`/${item.id}`"
       active-class="text-primary"
