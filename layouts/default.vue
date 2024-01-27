51cc4121220e912683cfb4af7d673753bb571c0b
Ilya <85136486+UchVuch@users.noreply.github.com>
2024-01-27 16:58:02 +0300

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,41 +13,43 @@ const links = [
 
 <template>
   <div>
-    <header class="header">
-      <div class="menu-links">
-        <ULink
-          v-for="link in links"
-          :key="link.to"
-          :to="link.to"
-          active-class="text-primary"
-          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
-        >
-          {{ link.name }}
-        </ULink>
-      </div>
-      <div class="user-controls">
-        <UButton
-          icon="i-heroicons-sun"
-          size="sm"
-          color="primary"
-          square
-          variant="solid"
-        />
-        <UButton
-          icon="i-heroicons-user"
-          size="sm"
-          color="primary"
-          square
-          variant="solid"
-        />
-      </div>
+    <header>
+      <nav class="nav">
+        <div class="menu-links">
+          <ULink
+            v-for="link in links"
+            :key="link.to"
+            :to="link.to"
+            active-class="text-primary"
+            inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+          >
+            {{ link.name }}
+          </ULink>
+        </div>
+        <div class="user-controls">
+          <UButton
+            icon="i-heroicons-sun"
+            size="sm"
+            color="primary"
+            square
+            variant="solid"
+          />
+          <UButton
+            icon="i-heroicons-user"
+            size="sm"
+            color="primary"
+            square
+            variant="solid"
+          />
+        </div>
+      </nav>
     </header>
     <slot />
   </div>
 </template>
 
 <style scoped>
-.header {
+.nav {
   display: flex;
   gap: 40px;
   align-items: center;
